02654dfc1ea04f90412a7e16cd8b0ee6f071f44d
Danis <danis.pojskic@disroot.org>
2024-05-22 23:13:12 +0200

    Dark mode button

--- assets/js/main.js
+++ assets/js/main.js
@@ -219,3 +219,15 @@ function openInNewTab(url) {
   var win = window.open(url, '_blank');
   win.focus();
 }
+
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const body = document.body;
+
+darkButton.onclick = () => {
+  body.classList.replace('light', 'dark');
+};
+
+lightButton.onclick = () => {
+  body.classList.replace('dark', 'light');
+};
